ff3c32ac0b246b527ad951fec94c9e1bae7a3ec1
Ryusei Noda <s23c1107nw@s.chibakoudai.jp>
2024-11-21 00:45:35 +0900

    make new branch

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,18 +1,31 @@
 import rclpy
 from rclpy.node import Node
+<<<<<<< HEAD
 from person_msgs.msg import Person
 
 rclpy.init()
 node =  Node("talker")
 pub = node.create_publisher(Person, "person", 10)
+=======
+from std_msgs.msg import Int16
+
+rclpy.init()
+node =  Node("talker")
+pub = node.create_publisher(Int16, "countup", 10)
+>>>>>>> 7c3505a... make new Repositories
 n = 0
 
 
 def cb():
     global n
+<<<<<<< HEAD
     msg = Person()
     msg.name = "野田琉晟"
     msg.age = n
+=======
+    msg = Int16()
+    msg.data = n
+>>>>>>> 7c3505a... make new Repositories
     pub.publish(msg)
     n += 1
 
--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
+<<<<<<< HEAD
 import os
 from glob import glob
+=======
+>>>>>>> 7c3505a... make new Repositories
 
 package_name = 'mypkg'
 
@@ -8,11 +11,18 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
+<<<<<<< HEAD
     data_files=[  
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('launch/*.launch.py'))
+=======
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+>>>>>>> 7c3505a... make new Repositories
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -23,6 +33,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+<<<<<<< HEAD
             'talker = mypkg.talker:main',
             'listener = mypkg.listener:main',
             'weather_publisher = mypkg.weather_publisher:main',
@@ -31,3 +42,10 @@ setup(
     },
 )
 
+=======
+            'talker = mypkg.talker:main', #talker.pyのmain関数という意味
+            #'listener = mypkg.listener:main', ←書いておいて後でコメントアウト
+        ],
+    },
+)
+>>>>>>> 7c3505a... make new Repositories
