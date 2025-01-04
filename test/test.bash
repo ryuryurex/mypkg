d4997d3876c7cb6dfff5851e46cb2fe395c84e39
Ryusei Noda <s23c1107nw@s.chibakoudai.jp>
2025-01-05 01:33:54 +0900

    fix test11

--- test/test.bash
+++ test/test.bash
@@ -2,17 +2,24 @@
 # SPDX-FileCopyrightText: 2025 Ryusei Noda
 # SPDX-License-Identifier: BSD-3-Clause
 
+
 dir=~
 [ "$1" != "" ] && dir="$1"
 
 cd $dir/ros2_ws
+
 colcon build
 
 source $dir/.bashrc
 
 timeout 30 ros2 launch mypkg talk_listen.launch.py > /tmp/weather_publisher.log
 
-cat /tmp/weather_publisher.log | grep 'temperature'
-cat /tmp/weather_publisher.log | grep 'windspeed'
-cat /tmp/weather_publisher.log | grep 'precipitation'
+count=$(cat /tmp/weather_publisher.log | grep -c 'weather_info_listener')
 
+if [ "$count" -ge 2 ]; then
+    echo "Test passed: Found $count entries."
+    exit 0
+else
+    echo "Test failed: Found only $count entries."
+    exit 1
+fi
