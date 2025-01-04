1e73dbe04ac6ac5e1fe0145c15996a0a5bb12646
Ryusei Noda <s23c1107nw@s.chibakoudai.jp>
2025-01-04 23:36:41 +0900

    fix

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2025 Ryusei Noda
+# SPDX-License-Identifier: BSD-3-Clause
+
 import launch
 import launch.actions
 import launch.substitutions
@@ -8,7 +11,7 @@ def generate_launch_description():
 
      talker = launch_ros.actions.Node(
          package='mypkg',      #パッケージの名前を指定
-         executable='talker',  #実行するファイルの指定
+         executable='weather_publisher.',  #実行するファイルの指定
          )
      listener = launch_ros.actions.Node(
          package='mypkg',
